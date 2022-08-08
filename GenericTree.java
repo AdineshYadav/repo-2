ea5b9bd8aceb4301f9f1273cac6f6b2caaf76ee4
AdineshYadav <86312918+AdineshYadav@users.noreply.github.com>
2022-08-08 22:31:37 +0530

    Add files via upload
    Generic Tree with basic inbuilt functionalities written in generic tree class

--- GenericTree.java
+++ GenericTree.java
@@ -53,4 +53,87 @@ public class GenericTree {
 
 
     }
+    public void display(){
+        System.out.println("----------------------------------");
+        display(root);
+        System.out.println("----------------------------------");
+    }
+
+    public int size(){
+        return size(root);
+    }
+
+    //Recursive function for displaying the generic tree
+     private void display(Node node){
+        String str = node.data + "->";
+
+        // loop for the child node present in the arrayList of the parent node
+        for(Node child : node.children){
+            str += child.data + " ";
+        }
+        str += ".";
+        System.out.println(str);
+
+        //smaller problem
+        for(Node child : node.children){
+            display(child);
+        }
+
+    }
+
+    //Function for finding the size of the generic tree (no. of nodes)
+    private int size(Node node){
+
+        int totalSize = 0;
+
+        //recursive call for every child present in the node
+        for(Node child : node.children){
+            //it will return the size of the respected subtree
+            int childSize = size(child);
+            //the size of the subtree will be added to the total size of the tree
+            totalSize += childSize;
+        }
+        return totalSize+1;
+    }
+
+    //Function to find the max node
+    private int max(Node node){
+        int totalMax = node.data;
+
+        //comparison within the subtree and assigning the maximum value
+        for(Node child : node.children){
+            int childMax = max(child);
+
+            if(childMax > totalMax){
+                totalMax = childMax;
+            }
+        }
+        return totalMax;
+    }
+
+    //Function for finding the node in binary tree
+    public boolean find(Node node , int item){
+        if(node.data == item){
+            return true;
+        }
+
+        for(Node child : node.children){
+            boolean childFind = find(child , item);
+           if(childFind){
+               return true;
+           }
+        }
+        return false;
+    }
+
+    public int height(Node node){
+        int totalHeight = -1;
+        for(Node child : node.children){
+            int childHeight = height(child);
+            if(childHeight > totalHeight){
+                totalHeight = childHeight;
+            }
+        }
+        return totalHeight+1;
+    }
 }
